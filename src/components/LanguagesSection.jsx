df356770cab19dcc3b6f3693d87543174edafad5
Seva <seva.leonov@hotmail.com>
2024-01-08 17:28:58 +0700

    add language styles and fix style bugs

--- src/components/LanguagesSection.jsx
+++ src/components/LanguagesSection.jsx
@@ -19,24 +19,27 @@ export default function LanguagesSection({
   return (
     <div className="language-section">
       <p>05 Languages</p>
-      {languages.map((language, index) => (
-        <div key={language.id} className="language">
-          <Input
-            label="Language"
-            placeholder="Language"
-            className="language name"
-            value={language.name}
-            onChange={(value) => onChange(index, "name", value, "languages")}
-          />
-          <Button
-            label="Remove"
-            className="remove-language"
-            onClick={() => {
-              removeItem("languages", language);
-            }}
-          />
-        </div>
-      ))}
+      <div className="languages">
+        {languages.map((language, index) => (
+          <div key={language.id} className="language">
+            <Input
+              label="Language"
+              placeholder="Language"
+              className="language-name"
+              value={language.name}
+              onChange={(value) => onChange(index, "name", value, "languages")}
+            />
+            <Button
+              label="Remove"
+              className="remove-language"
+              smallButton="true"
+              onClick={() => {
+                removeItem("languages", language);
+              }}
+            />
+          </div>
+        ))}
+      </div>
       <Button
         label="Add language"
         className="add-language"
